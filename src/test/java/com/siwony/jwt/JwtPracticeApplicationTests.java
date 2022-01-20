462fdf8003b76d8714f035561b1718c97a72472f
siwony_ <siwon103305@gmail.com>
2022-01-21 03:25:50 +0900

    Init project .java files

---
+++ src/test/java/com/siwony/jwt/JwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siwony.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
