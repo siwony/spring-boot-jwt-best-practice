8b466fab24aea9673abaa2db9ecef9a4a0130b2a
siwony_ <siwon103305@gmail.com>
2022-01-22 05:34:20 +0900

    Add member dto

---
+++ src/main/java/com/siwony/jwt/member/dto/MemberDto.java
@@ -0,0 +1,44 @@
+package com.siwony.jwt.member.dto;
+
+import com.siwony.jwt.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public final class  MemberDto {
+
+    @Getter
+    @AllArgsConstructor
+    public final static class Join {
+
+        @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
+        private final String email;
+
+        @NotBlank
+        private final String password;
+
+        @NotBlank
+        private final String name;
+
+        @Pattern(regexp = "01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$/")
+        private final String phonenumber;
+        
+        private Member toEntity(String encryptPassword){
+            return Member.builder()
+                    .email(this.email)
+                    .password(encryptPassword)
+                    .name(this.name)
+                    .phonenumber(this.phonenumber)
+                    .build();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public final static class LoginDto{
+        private final String email;
+        private final String password;
+    }
+}
