55afb8db950c23b4d5f810c65089c4031b2d72fb
siwony_ <siwon103305@gmail.com>
2022-01-24 16:58:05 +0900

    Add UserDetailsService 테스트 코드

--- src/main/java/com/siwony/jwt/member/Member.java
+++ src/main/java/com/siwony/jwt/member/Member.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
@@ -45,6 +47,18 @@ public class Member {
     @Builder.Default
     private List<Role> roles = new ArrayList<>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Member member = (Member) o;
+        return memberIdx != null && Objects.equals(memberIdx, member.memberIdx);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
 
 
